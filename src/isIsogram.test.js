481871a9452056ac7a87c1f26f018c2d8589cf13
Svitlana Chepa <svetachepa2320@gmail.com>
2026-02-19 10:26:34 +0000

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,34 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('returns true for an empty string', () => {
+    const result = isIsogram('');
+
+    expect(result).toBe(true);
+  });
+
+  it('returns true for a word without repetitions', () => {
+    const result = isIsogram('playgrounds');
+
+    expect(result).toBe(true);
+  });
+
+  it('returns false for a word with repeated letters', () => {
+    const result = isIsogram('look');
+
+    expect(result).toBe(false);
+  });
+
+  it(`is case-insensitive — treats 'Adam' as non-isogram`, () => {
+    const result = isIsogram('Adam');
+
+    expect(result).toBe(false);
+  });
+
+  it('ignores case', () => {
+    const result = isIsogram('Oops');
+
+    expect(result).toBe(false);
+  });
 });
